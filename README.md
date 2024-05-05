d286ffe6e7eed89db3602c62119df9dfebbcde19
tksnnx <tkser.edu@gmail.com>
2024-05-05 13:16:24 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # discord-dicebot
+
+## Usage
+
+1. Create an Application on [Developer Portal](https://discord.com/developers/applications).
+1. Set `MESSAGE CONTENT INTENT` to True from the **Bot** and issue a token.
+1. Select `scopes=bot` in the **OAuth2** URL Generator and select the following permissions.
+    - Read Messages/View Channels
+    - Send Messages
+    - Send Messages in Threads
+    - Read Message History
+1. Invite the bot to the server using the generated URL.
+1. Write `DISCORD_TOKEN` in the `.env` file.
+1. Execute the command below.
+
+```bash
+poetry install
+poetry run python app/main.py
+```
+
+or
+
+```bash
+docker build .
+```
+
+## LICENSE
+
+[MIT License](https://github.com/tksnnx/discord-dicebot/blob/main/LICENSE)
---
+++ app/main.py
@@ -0,0 +1,33 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from discord import app_commands
+import discord
+from dice import roll, DiceBaseException
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="/roll"))
+    await tree.sync()
+    print(f"Bot is ready as {client.user.name}")
+
+@tree.command(name="roll", description="Rolls a dice")
+@app_commands.describe(string="The dice to roll", private="Whether the result should be private")
+async def dice(interaction: discord.Interaction, string: str, private: bool = False):
+    try:
+        result = roll(string)
+        await interaction.response.send_message(f"Result: {result}", ephemeral=private)
+    except DiceBaseException as e:
+        await interaction.response.send_message(str(e), ephemeral=private)
+
+client.run(os.getenv("DISCORD_TOKEN"))
